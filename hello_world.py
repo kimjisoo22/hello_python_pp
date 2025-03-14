fd452f850b0d9819ae7a2bd6709daca201dac7ac
jisoo <d6169714@gmail.com>
2025-03-14 16:16:59 +0530

    adding a anew file

---
+++ hello_world.py
@@ -0,0 +1,5 @@
+def say_hello():
+    return "Hello ,World 2025!"
+
+if __name__ == "__main__":
+    print(say_hello())
---
+++ test_hello.py
@@ -0,0 +1,5 @@
+
+from hello_world import say_hello
+
+def test_say_hello():
+    assert say_hello() == "Hello ,World 2025!"
